12a7379997f822a57c8eef96ce50b8c27bb78ec3
Vologzhaninov <d.vologzhaninov@gmail.com>
2026-09-08 21:57:56 -0700

    Backend complete

---
+++ index.js
@@ -0,0 +1,71 @@
+import DOMPurify from 'dompurify'
+
+const form = document.querySelector('form');
+const userInput = document.getElementById('user-input');
+const inputLabel = document.getElementById('input-label');
+const fieldset = document.querySelector('fieldset');
+const button = document.querySelector('button');
+let language = '';
+let isFirstRender = true;
+
+form.addEventListener('submit', handleTranslate);
+
+function setLoading(isLoading) {
+    form.disabled = isLoading;
+}
+
+async function handleTranslate(event) {
+    event.preventDefault();
+    const data = new FormData(form);
+    isFirstRender ? language = data.get('lang'): null;
+    const userPrompt = userInput.value.trim();
+
+    if (!userPrompt) return;
+
+    setLoading(true);
+
+    //console.log(userPrompt)
+    //console.log(language)
+
+    try {
+        const response = await fetch (
+            '/api/translate', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                language: language, 
+                userPrompt: userPrompt})
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error(data.message)
+        }
+
+        //console.log(data);
+        let safeHTML = DOMPurify.sanitize(data.translation);
+        let outputHTML = 
+        `<label for="user-output" class="hand-point">
+            Your translation 👇
+        </label>
+        <textarea 
+            id="user-output" 
+            class="textarea"
+            rows="4" 
+            maxlength="100"
+            disabled
+        >${safeHTML}</textarea>`;
+        if (isFirstRender) {
+            inputLabel.textContent = "Original text 👇";
+            fieldset.classList.add("output-fieldset");
+            button.textContent = "Start Over";
+        }
+        fieldset.innerHTML = outputHTML;
+        isFirstRender = false;
+    } catch(error) {
+        console.error(error);
+    } finally {
+        setLoading(false);
+    }
+}
